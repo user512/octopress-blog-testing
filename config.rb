80e0fe372ee0d55a48599c5d6edef0dd5d5a677c
Tom Lee <tlee418@g.ucla.edu>
2015-07-05 22:53:20 -0700

    init

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/octopress-blog-testing/"
+http_images_path = "/octopress-blog-testing/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/octopress-blog-testing/fonts"
+css_dir = "public/octopress-blog-testing/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
